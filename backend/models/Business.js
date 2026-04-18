08f655e987f8fcb6f0e72c1c16aeec800dc73744
Beyzanrkya <l2311012073@ogr.sdu.edu.tr>
2026-04-18 17:39:53 +0300

    fix: Business model düzeltildi

--- backend/models/Business.js
+++ backend/models/Business.js
@@ -1,1 +1,16 @@
-l
+const mongoose = require("mongoose");
+
+const businessSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String },
+    address: { type: String },
+    description: { type: String },
+    categoryId: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Business", businessSchema);
